a55fb8eb5a063f6fc74e7ec4b65de2ba2cb727d7
MrBeelo <mrbeelomakris@gmail.com>
2026-07-07 21:33:24 +0300

    updated epic asteroid game for the newest zig version

--- build.zig
+++ build.zig
@@ -3,6 +3,10 @@ const std = @import("std");
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    var threaded: std.Io.Threaded = .init(std.heap.page_allocator, .{});
+    defer threaded.deinit();
+    const io = threaded.io();
     
     const raylib_dep = b.dependency("raylib", .{
         .target = target,
@@ -14,31 +18,26 @@ pub fn build(b: *std.Build) !void {
     
     const exe_mod = b.createModule(.{
         .target = target,
-        .optimize = optimize
-    });
-
-    const exe = b.addExecutable(.{
-        .name = "EpicAsteroidGame",
-        .root_module = exe_mod
+        .optimize = optimize,
+        .link_libc = true
     });
     
-    exe.linkLibrary(raylib_artifact);
+    exe_mod.linkLibrary(raylib_artifact);
     
-    exe.linkLibC();
     const include_cpp_opt = b.option(bool, "include-cpp", "Includes the C++ standard library");
-    if(include_cpp_opt orelse false) exe.linkLibCpp();
+    if(include_cpp_opt orelse false) exe_mod.link_libcpp = true;
     
     var sources = std.array_list.Managed([]const u8).init(b.allocator);
     const path = "src";
     {
-        var dir = try std.fs.cwd().openDir(path, .{ .iterate = true });
+        var dir = try std.Io.Dir.cwd().openDir(io, path, .{ .iterate = true });
         
         var walker = try dir.walk(b.allocator);
         defer walker.deinit();
     
         const allowed_exts = [_][]const u8{ ".c", ".cpp" };
     
-        while (try walker.next()) |entry| {
+        while (try walker.next(io)) |entry| {
             const ext = std.fs.path.extension(entry.basename);
             const include_file = for (allowed_exts) |e| {
             if (std.mem.eql(u8, ext, e))
@@ -55,15 +54,20 @@ pub fn build(b: *std.Build) !void {
     }
     
     for (sources.items) |src| {
-        exe.addCSourceFile(.{ .file = b.path(src)});
+        exe_mod.addCSourceFile(.{ .file = b.path(src)});
     }
     
     const sdk_path_opt = b.option([]const u8, "macos-sdk-path", "Path to macOS SDK");
     
     if (sdk_path_opt) |sdk_path| {
         const full_path = try std.fs.path.join(b.allocator, &[_][]const u8{sdk_path, "System/Library/Frameworks"});
-        exe.addSystemFrameworkPath(b.path(full_path));
+        exe_mod.addSystemFrameworkPath(b.path(full_path));
     }
+
+    const exe = b.addExecutable(.{
+        .name = "EpicAsteroidGame",
+        .root_module = exe_mod
+    });
     
     b.installArtifact(exe);
     
